d242c03ed0b4c49db13703d17ba23b0d03564240
harikrishnan.m <harikrishnan.m@marketsimplified.com>
2023-08-06 15:14:38 +0530

    Added some basics learned about constants and global vars

--- language-basics/variables.go
+++ language-basics/variables.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+const h int = 10 //This is allowed, Its called global var whose scope is within this file.
+// a:= 10 Won't work in global scope, because auto type inference is won't work in global scope in go lang.
+
 func main() {
 	a := 10
 	var b = 10
@@ -23,4 +26,9 @@ func main() {
 	e := &a // Address of the var 'a' is assigned to var 'e'
 	f := *e // The value at the memory address held by var 'e' is assigned to var 'f'
 	fmt.Println(e, *e, f)
+
+	const g uint = 100 //This is a constant, Constant's value must be know at compile time.
+	fmt.Println(g)
+
+	fmt.Printf("This is the const 'h' wiht value %v from gloable scope of this file.\n", h)
 }
